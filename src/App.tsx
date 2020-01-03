a71b54422c6862c87be4258623604ff361c42169
Amine Rehioui <amine@franticsoftware.com>
2020-01-03 11:57:03 -0500

    KitBasher wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,59 @@
 
 import * as React from "react";
-import { Button } from "@blueprintjs/core";
+import * as GoldenLayout from "golden-layout";
+import { Button, FocusStyleManager } from "@blueprintjs/core";
+import { NavBar } from "./NavBar";
 
 import "@blueprintjs/core/lib/css/blueprint.css";
 import "@blueprintjs/icons/lib/css/blueprint-icons.css";
 
+// TODO move this to async container
+import "../public/goldenlayout-base.css";
+import "../public/goldenlayout-dark-theme.css";
+
 export class App extends React.Component {
+    
+    private _layoutContainer!: HTMLDivElement;
+
+    public componentDidMount() {
+        FocusStyleManager.onlyShowFocusOnTabs();
+    }
+
     public render() {
+
+        const navbarWidth = "46px";
         return (
-            <div>
-                <Button intent="primary" text="Hello World" />
+            <div
+                style={{
+                    width: "100%",
+                    height: "100%",
+                    backgroundColor: "black"
+                }}
+            >
+                <div
+                    className="bp3-dark"
+                    style={{
+                        position: "absolute",
+                        left: 0,
+                        top: 0,
+                        width: "100%",
+                        height: "100%",
+                        overflow: "hidden"                      
+                    }}
+                >
+                    <NavBar width={navbarWidth} />
+                    <div 
+                        ref={e => this._layoutContainer = e as HTMLDivElement}
+                        style={{
+                            width: `calc(100% - ${navbarWidth})`,
+                            height: "100%",
+                            float: "left",
+                            zIndex: 0
+                        }}
+                    />
+                </div>
             </div>
         );
     }
+
 }
---
+++ src/NavBar.tsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+
+interface INavBarProps {
+    width: string;
+}
+
+export class NavBar extends React.Component<INavBarProps, {}> {
+
+    public render() {
+        return (
+            <div
+                style={{
+                    backgroundColor: "#182026",
+                    float: "left",
+                    width: this.props.width,
+                    height: "100%",
+                    zIndex: 2,
+                    position: "relative",
+                    paddingTop: "15px"
+                }}
+            >
+                <div className="pt-button-group pt-vertical">                    
+                    
+                </div>
+            </div>
+        );
+    }
+}
