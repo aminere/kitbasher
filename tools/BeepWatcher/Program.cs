fa44f005bca842e84f9d4830cf176b4833f5c67f
Amine Rehioui <amine@franticsoftware.com>
2020-01-21 15:54:36 -0500

    add beep watcher

---
+++ tools/BeepWatcher/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BeepWatcher
+{
+    class Program
+    {
+        static ManualResetEvent _quitEvent = new ManualResetEvent(false);
+        static void Main(string[] args)
+        {
+            var watcher = new FileSystemWatcher();
+            watcher.Path = ".";
+            watcher.NotifyFilter = NotifyFilters.LastWrite;
+            watcher.Filter = "*.js";
+            watcher.Changed += new FileSystemEventHandler((object sender, FileSystemEventArgs e) =>
+            {
+                Console.WriteLine(string.Format("{0} changed at {1}", e.FullPath, DateTime.Now.ToShortTimeString()));
+                SystemSounds.Beep.Play();
+            });
+            Console.WriteLine(string.Format("Watching changes in {0}...", watcher.Path));
+            watcher.EnableRaisingEvents = true;
+
+            Console.CancelKeyPress += (sender, eArgs) =>
+            {
+                _quitEvent.Set();
+                eArgs.Cancel = true;
+            };
+
+            _quitEvent.WaitOne();
+        }
+    }
+}
