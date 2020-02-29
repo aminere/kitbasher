2839430fe63f3c4e5dc181e2531ef70013522359
Amine Rehioui <amine@franticsoftware.com>
2020-02-29 10:47:29 -0500

    palette slot setter wip

--- src/components/MaterialEditor.tsx
+++ src/components/MaterialEditor.tsx
@@ -11,7 +11,14 @@ export class MaterialEditor extends React.Component<IMaterialEditorProps> {
         const { material } = this.props;
         return (
             <div>
-                
+                 {/* const entity = this._mockState.selection[0].children[0];
+                                                    const visual = entity.getComponent(Visual) as Visual;
+                                                    return visual.material as Material;
+													
+													
+													const elem = State.instance.selectedKit?.model.elements.data[0].instance;
+                                                const material = (elem as ModelMesh).material.asset;
+                                                return material as Material; */}
             </div>
         );
     }
